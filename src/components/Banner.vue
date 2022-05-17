170667bdea151a5e472c2486887c7745be1ed150
wendy03 <wendysu03@gmail.com>
2022-05-17 22:36:38 +0800

    feat(banner.vue):add login & register banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="col-md-6 mr-9 d-md-block d-none align-self-center">
+    <img
+      src="@/assets/img/banner.png"
+      alt="signin"
+      class="img-fluid d-block m-auto"
+    />
+  </div>
+</template>
