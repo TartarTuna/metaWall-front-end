b3b20f0e89d0d3e5e26fbe378c3c0f6717938da1
wendy03 <wendysu03@gmail.com>
2022-05-17 22:35:14 +0800

    feat(main.js):add axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from '@/App.vue'
 import router from '@/router'
 import '@/assets/scss/all.scss'
 import 'bootstrap'
 
 const app = createApp(App)
-
+app.use(VueAxios, axios)
 app.use(router)
 app.mount('#app')
