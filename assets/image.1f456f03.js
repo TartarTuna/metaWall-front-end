46ad882bb14c1627ed12385e5ea00204cac5e84f
TartarTuna <TartarTuna@users.noreply.github.com>
2022-05-24 01:13:10 +0000

    deploy: 6afc925e1733df6b7f0356092864633c01c22850

--- assets/image.1f456f03.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./index.940f7d9d.js";const o={"Content-Type":"multipart/form-data"},e=t=>a.post("/images",t,{headers:o});export{e as p};
--- assets/post.33086e49.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index.940f7d9d.js";const e=s=>t.get("/posts",{params:s}),p=s=>t.post("/posts",s),c=(s,o)=>t.patch(`/posts/${s}`,o),n=s=>t.delete(`/posts/${s}`);export{p as a,n as d,e as g,c as p};
