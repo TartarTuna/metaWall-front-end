e92eedef7a860dfe83eb9fffc4c7355503a0c301
TartarTuna <TartarTuna@users.noreply.github.com>
2022-05-26 08:50:18 +0000

    deploy: 461f7b4e212eb75e37e26a54b7f46c98b2350769

---
+++ assets/track.782363d0.js
@@ -0,0 +1,1 @@
+import{i as s}from"./index.c1cff9bf.js";const c=()=>s.get("/tracks"),e=t=>s.post(`/tracks/${t}`),r=t=>s.delete(`/tracks/${t}`);export{r as d,c as g,e as p};
--- assets/track.deade85a.js
+++
@@ -1,1 +0,0 @@
-import{i as s}from"./index.9169eb01.js";const c=()=>s.get("/tracks"),e=t=>s.post(`/tracks/${t}`),r=t=>s.delete(`/tracks/${t}`);export{r as d,c as g,e as p};
