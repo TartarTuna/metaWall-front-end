f2618ecd959cfaf72d50a82864f0c94867e18ac1
hsinyun <ruby8941@gmail.com>
2022-05-27 19:20:00 +0800

    [socket] add socket in wall.vue,app.vue when new comment,post

---
+++ src/components/NotificationCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card border-2 shadow-pushcard border8px">
+    <router-link
+      class="card-body py-2"
+      :to="to"
+    >
+      <p class="card-text text-muted text-center">
+        {{ text }}
+      </p>
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps({
+  to: {
+    type: Object,
+    default: null
+  },
+  text: {
+    type: String,
+    default: ''
+  }
+})
+
+const { to, text } = toRefs(props)
+</script>
