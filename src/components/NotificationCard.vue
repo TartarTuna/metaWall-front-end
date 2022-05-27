3add18d178a6a92ccb352435ede45a22e9ccc7dd
hsinyun <ruby8941@gmail.com>
2022-05-27 23:50:56 +0800

    [component] change Notification Card style

--- src/components/NotificationCard.vue
+++ src/components/NotificationCard.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="card border-2 shadow-pushcard border8px">
+  <div
+    class="card border-2 shadow-pushcard border4px"
+    style="width: 130px"
+  >
     <router-link
-      class="card-body py-2"
+      class="card-body py-1 px-1"
       :to="to"
     >
-      <p class="card-text text-muted text-center">
+      <p
+        class="card-text text-muted text-center"
+        style="font-size: 0.75rem"
+      >
         {{ text }}
       </p>
     </router-link>
