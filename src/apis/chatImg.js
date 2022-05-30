8b521e3a6153b173857a2b247dbfcba612d91097
akizyfee <87452790+akizyfee@users.noreply.github.com>
2022-05-30 21:35:31 +0800

    fix Rightbar.vue

--- src/apis/chatImg.js
+++ src/apis/chatImg.js
@@ -1,3 +1,7 @@
 import axios from '@/plugins/axios'
 
+<<<<<<< HEAD
 export const getChatImg = () => axios.get('/chatImg')
+=======
+export const getChatImg = () => axios.get('/chatImg')
+>>>>>>> 09fd902 (fix Rightbar.vue)
