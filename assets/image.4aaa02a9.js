0ba7f7800293516c89930fdd6bedb5c1d795eaac
TartarTuna <TartarTuna@users.noreply.github.com>
2022-06-08 01:28:22 +0000

    deploy: fc746362dfbed2b645e125f7e51f62f0975940b1

--- assets/image.4aaa02a9.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./index.f1e836e0.js";const o={"Content-Type":"multipart/form-data"},e=t=>a.post("/images",t,{headers:o});export{e as p};
---
+++ assets/image.b7591ff8.js
@@ -0,0 +1,1 @@
+import{T as a}from"./index.1437aae2.js";const o={"Content-Type":"multipart/form-data"},e=t=>a.post("/images",t,{headers:o});export{e as p};
